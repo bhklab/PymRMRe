8722ec2e09fa33745f398b5cae210bb2f7e00c98
clareli9 <li1033889482@gmail.com>
2019-07-17 09:31:05 -0400

    [Update] Finish the Python wrapper

--- MrmreData.py
+++ MrmreData.py
@@ -6,209 +6,279 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
 
 class MrmreData:
-    
+
+    # Data type constants
+    NUMERIC  = 0
+    FACTOR   = 1
+    TIME     = 2
+    EVENT    = 3
+
     def __init__(self,
                  data : pd.DataFrame = None,
-                 strata : np.array = None,
-                 weights : np.array = None,
+                 strata : pd.Series = None,
+                 weights : pd.Series = None,
                  priors : np.array = None):
 
         # Import the necessary r libraries needed for mRMRe
         self.survival = importr('survival')
 
-        self._data = np.array()
-        ## The case when features are too many
+        ## Declare the private or protected variables here
+        self.__data           = pd.DataFrame()
+        self.__feature_types  = pd.Series()
+        self.__strata         = pd.Series()
+        self.__weights        = pd.Series()
+        self.__priors         = np.array()
+        self.__sample_names   = list()
+        self.__feature_names  = list()
+
+        if not isinstance(data, pd.DataFrame):
+            raise Exception('Data must be of type dataframe')
         if data.shape[1] > (math.sqrt(2^31) - 1):
             raise Exception("Too many features, the number of features should be <= 46340")
  
-        # Check the data type of features
-        self._feature_types = []
+        # Define the feature types of the data
         for _, col in data.iteritems():
             # Firstly check whether the feature is survival data
             if col.name == 'time':
-                self._feature_types.append(2)
+                self.__feature_types.append(pd.Series([self.TIME]))
                 continue
             elif col.name == 'event':
-                self._feature_types.append(3)
+                self.__feature_types.append(pd.Series([self.EVENT]))
                 continue
-            
             # If not, check the feature is numeric data or categorical data (ordered-factor)
             if np.issubdtype(col.dtype, np.number):
-                self._feature_types.append(0)
+                self.__feature_types.append(pd.Series([self.NUMERIC]))
             elif col.dtype.name == 'category':
-                self._feature_types.appebd(1)
+                self.__feature_types.append(pd.Series([self.FACTOR]))
+            else:
+                raise Exception("Wrong labels")
 
-        ## Build the mRMR data
-        # All features are numeric type, convert the dataframe to matrix directly
-        if sum(feature_types) == 0:
-            self._data = data.values
-        # The feature data include categorical and survival data
-        ## Bo: There may exist some problem here (Surv). Why we do not need to create Surv here?
-        # Finally the self._data is matrix type, not dataframe
+        self.__sample_names = list(data.index.values)
+        self.__feature_names = list(data.column.values)
+
+        # Build the mRMR data
+        if self.__feature_types.sum() == 0:
+            self.__data = data
         else:
-            for i in range(len(feature_types)):
-                if feature_types[i] == 0:
-                    feature = pd.to_numeric(data.iloc[i])
-                elif feature_types[i] == 2 or feature_types[i] == 3:
-                    feature = data.iloc[i]
+            for i, __feature_type in self.__feature_types.iteritems():
+                if __feature_type == self.NUMERIC:
+                    # With the column name
+                    __feature = pd.to_numeric(data.iloc[:, i])
+                elif __feature_type in (self.TIME, self.EVENT):
+                    __feature = data.iloc[:, i]
                 else:
-                    feature = data.iloc[i].astype(int)
+                    # Why minus one? Is the indexing problems between R and C++?
+                    __feature = data.iloc[:, i].astype(int) - 1
 
-                self._data = np.hstack(self._data, feature)
+                self.__data[__feature.name] = __feature
 
-        # Sample Stratum Processing
-        if not strata:
-            self._strata = np.zeros(data.shape[0])
-        else:
-            self._strata = strata
+        # Sample Stratum processing
+        self.__strata = strata if strata else pd.Series(np.zeros(data.shape[0]))
 
-        # Sample Weight Processing
-        if not weights:
-            self._weights = np.ones(data.shape[0])
-        else:
-            self._weights = weights
+        # Sample Weight processing
+        self.__weights = weights if weights else pd.Series(np.ones(data.shape[0]))
 
-        # Prior Feature Matrix Processing
-        if priors:
-            self._priors = priors
+        # Sample Feature Matrix Processing
+        self.__priors = priors
 
         # No explictly return in __init__ function
 
     def featureData(self):
-        # Apply Surv here
-        # return dataframe here?
+        '''
+        ## Apply one Surv class here to build Surv object
+        :return: one dataframe with feature data
+        '''
+        ## Still need to figure out what it want to return
         for i in range(self._data.shape[1]):
             if self._feature_types[i] == 0:
+
+
+
         return
 
-    def subsetData(self, 
+    def subsetData(self,
                    row_indices : list = None, 
                    col_indices : list = None):
-        
+        '''
+        :param row_indices:
+        :param col_indices:
+        :return: A subset of mRMR data
+        '''
         if not row_indices and not col_indices:
-            return self._data 
+            return self.__data
         
         if not row_indices:
-            row_indices = list(range(self.sampleCount))
+            row_indices = list(range(self.sampleCount()))
         if not col_indices:
-            col_indices = list(range(self.featureCount))
+            col_indices = list(range(self.featureCount()))
 
-        data = self.featureData.iloc[row_indices, col_indices]
-        strata = 
-        
+        _data = self.featureData().iloc[row_indices, col_indices]
+        _strata = pd.factorize(self.sampleStrata().iloc[row_indices], sort = True)
+        _weights = self.sampleWeights().iloc[row_indices]
+        _priors = self.priors()[col_indices, col_indices] if self.priors() else None
+
+        return self.__init__(_data, _strata, _weights, _priors)
+
 
     ## SampleCount
     def sampleCount(self):
-        return self._data.shape[0]
+        '''
+        :return:
+        '''
+        return self.__data.shape[0]
 
     ## SampleName
     def sampleNames(self):
-        return 
+        '''
+        :return:
+        '''
+        return self.__sample_names
     
     ## featureCount
     def featureCount(self):
-        return len(self._feature_names)
+        '''
+        :return:
+        '''
+        return len(self.__feature_names)
     
     ## featureNames
-    def featureCount(self):
-        return 
+    def featureNames(self):
+        '''
+        :return:
+        '''
+        return self.__feature_names
     
     ## sampleStrata
     def sampleStrata(self, value : pd.Series = None):
+        '''
+        :param value:
+        :return:
+        '''
         if not value:
-            return self._strata
-        if len(value) != self._data.shape[0]:
-            raise Exception('Data and strata must contain the same number of samples')
-        elif value.dtype.name == 'category':
-            raise Exception('Strata must be provided as factors')
+            _strata = self.__strata
+            _strata.index = list(self.__data.index.values)
+            return _strata
+
         else:
-            self._strata = value.astype(int) - 1
+            if len(value) != self.__data.shape[0]:
+                raise Exception('Data and strata must contain the same number of samples')
+            elif value.dtype.name != 'category':
+                raise Exception('Strata must be provided as factors')
+            elif value.isnull().any().any():
+                raise Exception('Cannot have missing value in strata')
+            # Why we need to return self variable in this case?
+            # Do we need to minus one here?
+            self.__strata = value.astype(int)
 
     ## SampleWeights
     def sampleWeights(self, value : pd.Series = None):
+        '''
+        :param value:
+        :return:
+        '''
         if not value:
-            return self._weights
-        if len(value) != self._data.shape[0]:
-            raise Exception('Data and weight must contain the same number of samples')
-        elif value.isnull.values.any():
-            raise Exception('cannot have missing values in weights')
+            _weights = self.__weights
+            _weigths.index = list(self.__data.index.values)
+            return _weights
         else:
-            self._weights = value.astype(float)
+            if value.size != self.__data.shape[0]:
+                raise Exception('Data and weight must contain the same number of samples')
+            elif value.isnull.any().any():
+                raise Exception('cannot have missing values in weights')
+            
+            self.__weights = value.astype(float)        
 
     ## Priors
     def priors(self, value):
-        if value.shape[0] != self._data.shape[0] or value.shape[1] != self._data.shape[1]:
-            raise Exception('Priors matrix must be a symmetric matrix containing as many features as data')
-        
-        self._priors = self.expandFeatureMatrix(value)
-    
+        '''
+        :param value: A numpy matrix
+        :return: 
+        '''
+        if not value:
+            return self.__compressFeatureMatrix(self.__priors) if self.__priors else None
+        else:
+            if value.shape[0] != self._data.shape[0] or value.shape[1] != self._data.shape[1]:
+                raise Exception('Priors matrix must be a symmetric matrix containing as many features as data')
+            self.__priors = self.__expandFeatureMatrix(value)
+
     ## Mutual information matrix
-    def mim(self, prior_weight = 0, continuous_estimator, outX = True, bootstrap_count):
+    def mim(self, prior_weight = 0, continuous_estimator = None, outX = True, bootstrap_count = 1):
         if continuous_estimator not in ['pearson', 'spearman', 'kendall', 'frequency']:
             raise Exception('The continuous estimator should be one of pearson, spearman, kendall and frequency')
-        if len(self._priors) != 0:
-            if prior_weight < 0 or prior_weight > 1:
-                raise Exception('prior weight must be a value ranging from 0 to 1')
+        if self.__priors:
+            if not prior_weight:
+                raise Exception('Prior weight must be provided if there are priors')
+            elif prior_weight < 0 or prior_weight > 1:
+                raise Exception('Prior weight must be a value ranging from 0 to 1')
         else:
             prior_weight = 0
+        
+        _mi_matrix = 
         '''
         call the cpp function
         '''
-        return
+        return _mi_matrix
 
     ## expandFeatureMatrix
-    def expandFeatureMatrix(self, matrix):
-        adaptor = np.where(np.array(self._feature_types) != 3)[0]
-        # ???????
-        matrix = matrix.tolist()
-        for i in range(len(adaptor)):
-            for j in range(len(adaptor)):
-                #item = matrix[j][i]
-                if self._feature_types[adaptor[j]] == 2:
-                     
+    def __expandFeatureMatrix(self, matrix):
+        _expanded_matrix = np.array()
+        _adaptor = self.__feature_types.index[self.__feature_types != 3].tolist()
+        for i in range(len(_adaptor)):
+            col = np.array()
+            for j in range(len(_adaptor)):
+                # item should be relevant to adaptor?
+                item = matrix[_adaptor[j]][_adaptor[i]]
+                if self.__feature_types[_adaptor[j]] == 2:
+                    col = col.vstack([item], [item])
+                else:
+                    col = col.vstack([item])
+            if self.__feature_types[_adaptor[i]] == 2:
+                expanded_matrix.hstack(col)
+                expanded_matrix.hstack(col)
+            else:
+                expanded_matrix.hstack(col)
 
-
-
-        return 
+        return expanded_matrix
         
     ## compressFeatureMatrix
-    def compressFeatureMatrix(self, matrix):
-        # DONE
-        adaptor = np.where(np.array(self._feature_types != 3))[0]
+    def __compressFeatureMatrix(self, matrix):
+        
+        _adaptor = self.__feature_types.index[self.__feature_types != 3].tolist()
 
-        return matrix[adaptor, adaptor]
+        return matrix[_adaptor, _adaptor]
 
     ## expandFeatureIndices
-    def expandFeatureIndices(self, indices):
-        adaptor = np.where(np.array(self._feature_types == 3))[0]
-        if len(adaptor) > 0 and (indices >= adaptor).any():
+    def __expandFeatureIndices(self, indices):
+        # Compare the list and array? 
+        _adaptor = self.__feature_types.index[self.__feature_types == 3].tolist()
+        if len(_adaptor) > 0 and (indices >= _adaptor).any():
             for i in range(len(indices)):
-                temp = 0
-                for j in range(len(adaptor)):
+                for j in range(len(_adaptor)):
                     # 0/1 Indexing problem? Why plus one here?
-                    if indices[i] >= adaptor[j] - j + 1:
-                        temp += 1 
-                indices[i] += temp
-                temp = 0
-        
+                    indices[i] += (indices[i] >= (_adaptor[j] - j + 1))
+
         return indices
 
     ## compressFeatureIndices
-    def conpressFeatureIndices(self, indices):
-        adaptor = np.where(np.array(self._feature_types == 3))[0]
-        if len(adaptor) > 0:
+    def __compressFeatureIndices(self, indices):
+        indices = np.array(indices)
+        _adaptor = self.__feature_types.index[self.__feature_types == 3].tolist()
+        # It's correct here
+        if len(_adaptor) > 0:
             for i in range(len(indices)):
-                temp = 0
-                for j in range(len(adaptor)):
-                    if indices[i] >= adaptor[j]:
-                        temp += 1
-                indices[i] -= temp
-                temp = 0
+                for j in range(len(_adaptor)):
+                    indices[i] -= (indices[i] >= _adaptor[j])
+
         return indices
 
     def scores(self, solutions):
+        '''
+        Better develop that after the finish of mRMR.Filter class
+        '''
         mi_matrix = self.mim()
-        # ???
+        targets = 
+
+
 
 
         return 
--- MrmreFilter.py
+++ MrmreFilter.py
@@ -13,7 +13,8 @@ class MrmreFilter:
                  continous_estimator : str,
                  outX : bool,
                  bootstrap_count : int):
-        self._method = method 
+
+        self._method = method
         self._continous_estimator = continuous_estimator
 
         if type(data) != 'mRMRe_data':
@@ -29,7 +30,7 @@ class MrmreFilter:
 
         ## Target processing
 
-        if any(x < 1 for x in target_indices) or any(x > data.featureCount for x in target_indices):
+        if any(x < 1 for x in target_indices) or any(x > data.featureCount() for x in target_indices):
             raise Exception('target indices must only contain values ranging from 1 to the number of features in data')
         
         ## Level processing
@@ -40,16 +41,17 @@ class MrmreFilter:
         self._target_indices = target_indices.astype(int)
         self._levels = levels.astype(int)
 
+        # The index 0/1 problems?
         target_indices = data.expandFeatureIndices(target_indices).astype(int) - 1
 
         ## Filter; Mutual Information and Causality Matrix
 
-        mi_matrix = np.zeros((data._nrow, data_ncol))
+        mi_matrix = np.zeros((data._nrow, data._ncol))
 
         if method == 'exhaustive':
 
             ## Level processing
-            if np.prod(levels) > comb(data._featureCount - 1, len(levels)):
+            if np.prod(levels) - 1 > comb(data._featureCount - 1, len(levels)):
                 raise Exception('user cannot request for more solutions than is possible given the data set')
 
             '''
@@ -65,8 +67,21 @@ class MrmreFilter:
             raise Exception('Unrecognized method: use exhaustive or bootstrap')
 
 
-        ## 
-        self._filters = 
+        ## After building the result, result is the data structure of Result defind in exports.h
+        ## The returned filter is object of list
+        self._filters = []
+        for i in range(len(result.solutions):
+            sol_matrix = (data.compressFeatureIndices(result.solutions[i] + 1)).reshape(len(self._levels), np.prod(self._levels))
+            self._filter.append(sol_matrix)
+        self._causality_list = list(result.casuality)
+
+        self._scores = [i.reshape(len(self._levels), np.prod(self._levels)) for i in result.scores]
+        _, cols_unique = np.unique(data.compressFeatureIndices(list(range(data._ncol))))
+        # Do we need to store the names of targets?
+        self._causality_list = [causality[cols_unique] for causality in self._causality_list]
+        self._mi_matrix = data.compressFeatureMatrix(mi_matrix.reshape(data._ncol, data._ncol))
+
+        # Ignore the feature names / sample names
 
     
 
@@ -89,5 +104,39 @@ class MrmreFilter:
     def solutions(self, mi_threshold = -float('inf'), causality_threshold = float('inf')):
         # filters[target][solution, ] is a vector of selected features
         # in a solution for a target; missing values denote removed features
+        ## One question is why we need the string here?
+        filters = []
+        for target_index in self._target_indices:
+            res
+
+
+            result_matrix = self._filters[target_index]
+            ### numpy where return a turple, the first element is the indices array
+            causality_dropped = np.where(np.array(self._casuality_list[str(target_index)] > causality_threshold))[0]
+            mi_dropped = np.where(np.array(-.5 * np.log(1 - np.square(self._mi_matrix[:,target_index])) < mi_threshold))[0]
+            # Need to apply the Nan operation here?
+            pre_return_matrix = np.flip(result_matrix, axis = 1)
+        filters.append(pre_return_matrix)
+
+        return filters
+
+    def scores(self):
+        mi_matrix = self.mim()
+        # No need to use the string style of targets, it is for the names of columns in R
+        targets = self._target_indices
+
+        scores = []
+        for target in targets:
+
+
+
+        return
+
+    def mim(self, method):
+        ## The method should be within mi or cor
+        return self._mi_matrix
+
+    def target(self):
+        return self._target_indices
 
         
