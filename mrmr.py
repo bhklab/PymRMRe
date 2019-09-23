910dd89622039b4772e519e2f374005b98504ca2
clareli9 <li1033889482@gmail.com>
2019-09-23 11:13:08 -0400

    [Update] Process target indices

--- mrmr.py
+++ mrmr.py
@@ -7,12 +7,13 @@ from MrmreFilter import *
 
 def mrmr_selection(target_df : pd.DataFrame,
                    features_df : pd.DataFrame,
+                   target_features : list,
                    feature_count : int,
                    solution_count : int = 1,
                    method : str = 'exhaustive',
                    estimator : str = 'pearson',
                    survival : bool = False):
-    ## Data Pre-processing
+    # Data Pre-processing
     target = target_df.copy()
     if not survival:
         target['time'] = target['target']
@@ -21,10 +22,15 @@ def mrmr_selection(target_df : pd.DataFrame,
     target.set_index('id', inplace = True)
     features = features_df.merge(target[['time', 'event']], how = 'inner', left_index = True, right_index = True)
     
-    ## Build the mRMR Data
+    # Build the mRMR Data
     mrmr_data = MrmreData(data = features)
     
-    ## Build the mRMR Filter
+    # Find the target_indices
+    target_indices = []
+    for feature in target_features:
+        target_indices.append(features.columns.get_loc(feature))
+
+    # Build the mRMR Filter
     levels = [solution_count] + [1] * features_count
     mrmr_filter = MrmreFilter(data = mrmr_data, target_indices = target_indices, levels = levels)
     
