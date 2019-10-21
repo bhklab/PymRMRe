cfbb33b6ca1aae2d130fdd70f651aaec8f2014f7
clareli9 <li1033889482@gmail.com>
2019-10-20 23:55:32 -0400

    [Update] Add the fixed selected features

--- mrmr.py
+++ mrmr.py
@@ -5,7 +5,42 @@ import os
 from MrmreData import *
 from MrmreFilter import *
 
-def mrmr_selection(target_df : pd.DataFrame,
+def mrmr_selection(features : pd.DataFrame,
+                   target_features : list,
+                   target_count : int,
+                   solution_count : int = 1,
+                   fixed_selected_count : int = 0,
+                   method : str = 'exhaustive',
+                   estimator : str = 'pearson',
+                   survival : bool = False):
+    # No need for data pre-processing, since the data frame input is already the combined version
+    if not survival:
+        features['time'] = features['target']
+        features['event'] = 1
+    
+    features.set_index('id', replace = True)
+    features = features.infer_objects()
+
+    # Build the mRMR data
+    mrmr_data = MrmreData(data = features)
+
+    # Find the target indices and fixed features selected
+    target_indices = []
+    for tf in target_features:
+        target_indices.append(features.columns.get_loc(tf))
+
+    fixed_selected_indices = list(range(fixed_selected_count))
+    
+    # Build the mRMR Filter
+    levels = [solution_count] + [1] * target_count
+    mrmr_filter = MrmreFilter(data = mrmr_data, target_indices = target_indices, fixed_selected_count = fixed_selected_count,
+                              levels = levels)
+    
+    return mrmr_filter._filter
+
+
+
+def mrmr_selection_with_clinical(target_df : pd.DataFrame,
                    features_df : pd.DataFrame,
                    target_features : list,
                    feature_count : int,
@@ -21,7 +56,8 @@ def mrmr_selection(target_df : pd.DataFrame,
     
     target.set_index('id', inplace = True)
     features = features_df.merge(target[['time', 'event']], how = 'inner', left_index = True, right_index = True)
-    
+    features = features.infer_objects()
+
     # Build the mRMR Data
     mrmr_data = MrmreData(data = features)
     
