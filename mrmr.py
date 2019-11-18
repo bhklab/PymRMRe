794567f3e804923ab66e406e3b77bca494a3ab58
clareli9 <li1033889482@gmail.com>
2019-11-18 11:12:36 -0500

    [Update] Add the function wrapper

--- mrmr.py
+++ mrmr.py
@@ -7,20 +7,20 @@ from MrmreFilter import *
 
 def mrmr_selection(features : pd.DataFrame,
                    target_features : list,
-                   features_types : list,
-                   target_count : int,
+                   feature_types : list,
+                   solution_feature_count : int,
                    solution_count : int = 1,
-                   fixed_selected_count : int = 0,
+                   fixed_feature_count : int = 0,
                    method : str = 'exhaustive',
                    estimator : str = 'pearson',
                    survival : bool = False):
 
     ## Handle some corner cases
     # The input features type
-    if len(features_types) != features.shape[1]:
+    if len(feature_types) != features.shape[1]:
         raise Exception('The count of feature types does not match with the size of dataset')
     
-    if any((x < 0 or x > 3) for x in features_types):
+    if any((x < 0 or x > 3) for x in feature_types):
         raise Exception('The feature type should within the range 0, 1, 2, 3')
     
     # The method and estimator do not match
@@ -30,18 +30,26 @@ def mrmr_selection(features : pd.DataFrame,
     if estimator not in ["pearson", "spearman", "kendall", "frequency"]:
         raise Exception("The continuous estimator must be chosen from pearson, spearman, kendall and frequency")
 
+    # The length of one features larger than fixed one
+    if fixed_feature_count > solution_feature_count:
+        raise Exception("The count of fixed-selected features should be less than the expected count of one solution")
+
     
     # No need for data pre-processing, since the data frame input is already the combined version
+    ## No need to specify the survival option, since no matter what the target indices have to 
+    ## be the input
+    '''
     if not survival:
         features['time'] = features['target']
         features['event'] = 1
+    '''
     
-    features.set_index('id', replace = True)
+    #features.set_index('id', replace = True)
     features = features.infer_objects()
 
     # Build the mRMR data
     mrmr_data = MrmreData(data = features, 
-                          features_types = features_types)
+                          feature_types = feature_types)
 
     ## Here the 
     # Find the target indices and fixed features selected
@@ -49,16 +57,16 @@ def mrmr_selection(features : pd.DataFrame,
     for tf in target_features:
         target_indices.append(features.columns.get_loc(tf))
 
-    fixed_selected_indices = list(range(fixed_selected_count))
+    #fixed_selected_indices = list(range(fixed_selected_count))
     
     # Build the mRMR Filter
-    levels = [solution_count] + [1] * target_count
+    levels = [solution_count] + [1] * (solution_feature_count - fixed_feature_count - 1)
     mrmr_filter = MrmreFilter(data = mrmr_data, 
                               target_indices = target_indices, 
-                              fixed_selected_count = fixed_selected_count,
+                              fixed_selected_count = fixed_feature_count,
                               levels = levels)
-    
-    return mrmr_filter._filter
+
+    return mrmr_filter.solutions()
 
 def mrmr_selection_with_clinical(target_df : pd.DataFrame,
                    features_df : pd.DataFrame,
